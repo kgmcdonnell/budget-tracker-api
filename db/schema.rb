c14534c4dc6afad3fe32eb11677b158caffa6c44
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-07-06 14:01:58 -0400

    added User model

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_180031) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+end
